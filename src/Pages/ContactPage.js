5f753b451838c39acb0881511471655606e11ab5
Dawid T <geekoonn@gmail.com>
2023-08-10 18:28:12 +0200

    added map and first content to home tab

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -16,7 +16,19 @@ const ContactPage = () => {
       <div className="contactTitle">
         <h2>Skontaktuj się z nami!</h2>
       </div>
-      <div className="contactWrapper">{items}</div>
+      <div className="contactWrapper">
+        <div className="contactWrapperItems">{items}</div>
+        <div className="contactWrapperMap">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304.7177290027841!2d20.947705380090202!3d52.33882947596993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ec834c91e2073%3A0x38cf1d6a316ef46a!2sPoet%C3%B3w%2010C%2C%2003-147%20Warszawa!5e0!3m2!1spl!2spl!4v1691675603040!5m2!1spl!2spl"
+            width="600"
+            height="450"
+            style={{ border: "0" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
     </section>
   );
 };
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,66 @@
 import React from "react";
+import "../styles/Home.css";
 
 const HomePage = () => {
-  return <div>Zakładka Home</div>;
+  return (
+    <section className="home">
+      <div className="homeTitle">
+        <h2 className="homeTitleTitle">Viki Transcar</h2>
+        <p className="homeTitleText">
+          Wypożyczalnia samochodów osobowych, taxi i dostawczych
+        </p>
+        <div className="shadow"></div>
+      </div>
+      <section className="homeWrapper">
+        <div className="homeWrapperText">
+          <div>
+            <h3>Zajmujemy sie wynajmem samochodów:</h3>
+            <ul>
+              <li>osobowych,</li>
+              <li>osobowych na taxi,</li>
+              <li>dostawczych.</li>
+            </ul>
+            <p className="bold">na terenie Polski oraz za granicą.</p>
+          </div>
+          <div>
+            <h3>W zależności od Twoich potrzeb oferujemy wynajem:</h3>
+            <ul>
+              <li>krótkoterminowy,</li>
+              <li>długoterminowy,</li>
+              <li>jako samochód zastępczy (z OC sprawcy).</li>
+            </ul>
+            <p className="bold">
+              Wszystkie opłaty są u nas
+              <span className="capital">
+                {" "}
+                transparentne - nie mamy nic do ukrycia
+              </span>
+              .
+            </p>
+          </div>
+          <div>
+            <h2>TAXI</h2>
+            <p className="bold">
+              Jesteśmy partnerem rozliczeniowym kierowców taxi
+            </p>
+            <p className="bold">
+              Współpracujemy praktycznie ze wszystkimi korporacjami taxi w
+              Warszawie i nie tylko.
+            </p>
+            <p className="bold">
+              Największa ilość naszych kierowców jeździ dla m.in.:
+            </p>
+            <ul>
+              <li>FreeNow</li>
+              <li>iTaxi</li>
+              <li>Ele Taxi</li>
+              <li>Bolt</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default HomePage;
